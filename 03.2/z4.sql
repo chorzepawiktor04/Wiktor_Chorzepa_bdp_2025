e675dcf6d58a3993ec3000313dda420a125e5931
Wiktor Chorzępa <147316663+chorzepawiktor04@users.noreply.github.com>
2025-10-28 20:32:14 +0100

    Insert sample data into input_points table
    Added sample data insertion for input_points table.

--- 03.2/z4.sql
+++ 03.2/z4.sql
@@ -3,3 +3,7 @@ CREATE TABLE input_points (
     name VARCHAR(50),
     geometry GEOMETRY(Point, 4326)  -- WGS84
 );
+
+INSERT INTO input_points (name, geometry) VALUES
+('P1', ST_SetSRID(ST_MakePoint(8.36093, 49.03174), 4326)),
+('P2', ST_SetSRID(ST_MakePoint(8.39876, 49.00644), 4326));
